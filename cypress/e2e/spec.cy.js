4928cca872f6da44475efbe6c17e66c71eed336d
Aleksandra Mikszuta <ola.mikszuta@gmail.com>
2023-08-25 21:31:03 +0200

    Code formatting

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,40 +2,39 @@ import homeSelectors from '../selectors/home.sel'
 import loginSelectors from '../selectors/login.sel'
 import userHelpers from '../apiHelpers/user'
 
-function login(userName, userPassword){
-    cy.get(homeSelectors.loginLink).click()
-    cy.get(loginSelectors.loginWithEmailLink).click()
-    cy.get(loginSelectors.emailField).type(userName)
-    cy.get(loginSelectors.passwordField).type(userPassword)
-    return cy.get(loginSelectors.loginButton).click()
+function login(userName, userPassword) {
+  cy.get(homeSelectors.loginLink).click()
+  cy.get(loginSelectors.loginWithEmailLink).click()
+  cy.get(loginSelectors.emailField).type(userName)
+  cy.get(loginSelectors.passwordField).type(userPassword)
+  return cy.get(loginSelectors.loginButton).click()
 }
 
 describe('Studysmarter page', () => {
   let userName = null;
   let userPassword = 'sydriv-4pipwo-Ducdax';
 
-  before(function() {
+  before(function () {
     userName = userHelpers.registerUserTimestamp(userPassword)
   })
 
-  beforeEach(function() {
+  beforeEach(function () {
     cy.visit('https://demo.studysmarter-test.de');
   })
 
   it('should allow a registered user to log in', () => {
-      login(userName, userPassword)
+    login(userName, userPassword)
 
-      cy.contains('Welcome to StudySmarter!')
-      cy.contains('Get started')
+    cy.contains('Welcome to StudySmarter!')
+    cy.contains('Get started')
   })
 
   it('should allow a logged in user to create a study set', () => {
     login(userName, userPassword)
   })
 
-  it('should allow email registration via website', () => {      
-      cy.contains('Sign up with email').click()
+  it('should allow email registration via website', () => {
+    cy.contains('Sign up with email').click()
 
-   });
+  });
 });
-  
